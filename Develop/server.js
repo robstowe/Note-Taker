c6f792174b47e30f29e924fa7b61dbd5cdf35b22
robstowe <robstowe94@gmail.com>
2023-06-15 14:19:22 -0700

    add the uuid page

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,11 @@
-//i created this page, based on what we used in class thru Module 11
 
 const express = require('express');
 const path = require('path');
 const PORT = 3001;
 const app = express();
-const notes = require('./db/db.json') // todo: why do i need to pull from json file?
+const notes = require('./db/db.json');
+const uuid = require('./helpers/uuid');
+const bodyParser = require('body-parser');
 
 
 app.use(express.static('public'));
@@ -21,7 +22,7 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html')) //this takes you to the actual notes page
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', bodyParser.json(), (req, res) => {
   const { title, text } = req.body;
   if (title && text) {
     const newPost = {
@@ -34,7 +35,7 @@ app.post('/api/notes', (req, res) => {
       body: newPost,
     };
 
-    console.log(body);
+    console.log(req.body);
     res.status(201).json(response);
   }else {
     res.status(500).json('Error posting note');
