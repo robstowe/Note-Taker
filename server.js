610af03e0635388e034767e06375e83c2151945a
robstowe <robstowe94@gmail.com>
2023-06-20 14:42:26 -0700

    npm i and adding in commands to post and delete

--- server.js
+++ server.js
@@ -25,6 +25,33 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html')) //this takes you to the actual notes page
 });
 
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid();
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+      fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+          if (err) throw err;
+          res.json(notes);
+      });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      const newNotes = notes.filter((note) => note.id !== id);
+      fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 2), (err) => {
+          if (err) throw err;
+          res.json(newNotes);
+      });
+  });
+});
+
 // app.post('/api/notes', bodyParser.json(), (req, res) => {
 //   app.use(express.json());
 //   app.use(express.urlencoded({ extended: true }));
@@ -78,4 +105,6 @@ fetch ('/api/users', {
     mainEL.append(h1El)
   }
 });
+
+
     
