88c2b1a3db76d2c7ee638fd623cc2e1877f2584c
robstowe <robstowe94@gmail.com>
2023-06-20 14:46:27 -0700

    more code gone

--- server.js
+++ server.js
@@ -51,39 +51,7 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
-// app.post('/api/notes', bodyParser.json(), (req, res) => {
-//   app.use(express.json());
-//   app.use(express.urlencoded({ extended: true }));
-//   console.info(`${req.method} request recieved for notes`);
 
-//   const { title, text } = req.body;
-//   if (title && text) {
-//     const newPost = {
-//       title,
-//       text,
-//       post_id: uuid(),
-//     }
-    
-//   const reviewString = JSON.stringify(newPost);
-//   fs.readFile('./db/db.json', 'utf8', (error, data) => {
-//     console.log(JSON.parse(data));
-//   })
-//   fs.writeFile('./db/db.json', reviewString, (err) => {
-//     err
-//     ? console.error(err)
-//     : console.log(`Review for ${newPost.Title} has been written to JSON file`)
-//   });
-//   const response = {
-//     status: 'success',
-//     body: newPost,
-//   };
-
-//   console.log(req.body);
-//   res.status(201).json(response);
-// }else {
-//   res.status(500).json('Error posting note');
-// }
-// });  
 
 
 app.listen(PORT, () => {
